e3dd348ce3cd571a782a048f982bd5d894d142b8
Thomas Kellermeier <t-kellermeier@hotmail.de>
2018-04-25 10:58:24 +0200

    Initial gitignore and setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+requirements = []
+
+setup(
+    name='mp-2018',
+    author='Maxi Fischer, Willi Gierke, Ajay Kesar, Thomas Kellermeier, Axel Stebner, Daniel Thevessen',
+    description='Unsupervised Anomaly Detection: Representation Learning for Predictive Maintenance over Time',
+    long_description=open('README.md').read(),
+    version='0.0',
+    packages=[],
+    scripts=[],
+    install_requires=requirements,
+    url='github.com/KDD-OpenSource/MP-2018',
+    license='MIT License',
+)
