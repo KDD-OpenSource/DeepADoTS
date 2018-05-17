09c066ad3f63e73e77576edc01d72e8a78e254ab
Daniel Theveßen <danthe96@gmx.de>
2018-05-17 12:33:18 +0200

    Fix fit/predict signature

--- src/algorithms/rnn_ebm.py
+++ src/algorithms/rnn_ebm.py
@@ -10,16 +10,19 @@ class RecurrentEBM(Algorithm):
     The interface of the class is sklearn-like.
     """
 
-    def __init__(self, num_epochs=100, n_hidden=50, n_hidden_recurrent=100, min_lr=0.01):
+    def __init__(self, num_epochs=100, n_hidden=50, n_hidden_recurrent=100,
+                 min_lr=0.01, min_energy=None, batch_size=10):
         self.num_epochs = num_epochs
         self.n_hidden = n_hidden  # Size of RBM's hidden layer
         self.n_hidden_recurrent = n_hidden_recurrent  # Size of RNN's hidden layer
         self.min_lr = min_lr
+        self.min_energy = min_energy  # Threshold for anomaly
+        self.batch_size = batch_size
 
         # Placeholders
         self.input_data = None
         self.lr = None
-        self.batch_size = None
+        self._batch_size = None
 
         # Variables
         self.W, self.Wuh, self.Wux, self.Wxu, self.Wuu, self.bu, \
@@ -33,21 +36,21 @@ class RecurrentEBM(Algorithm):
         self.tf_session = None
         self.tf_saver = None
 
-    def fit(self, X, batch_size=10):
+    def fit(self, X, _):
         self._build_model(X.shape[1])
 
         self.tf_session = tf.Session()
         self._initialize_tf()
-        self._train_model(X, batch_size)
+        self._train_model(X, self.batch_size)
 
-    def predict(self, X, min_energy=None):
+    def predict(self, X):
         scores = []
         labels = []
 
         for i in range(len(X)):
             reconstruction_err = self.tf_session.run([self.cost],
                                                      feed_dict={self.input_data: X[i:i + 1],
-                                                                self.batch_size: 1})
+                                                                self._batch_size: 1})
             scores.append(reconstruction_err[0])
             if min_energy is not None:
                 labels.append(reconstruction_err[0] >= min_energy)
@@ -63,7 +66,7 @@ class RecurrentEBM(Algorithm):
                     alpha = min(self.min_lr, 0.1 / float(i + 1))
                     _, C = self.tf_session.run([self.update, self.cost],
                                                feed_dict={self.input_data: x, self.lr: alpha,
-                                                          self.batch_size: batch_size})
+                                                          self._batch_size: batch_size})
                     costs.append(C)
             print('Epoch: {} Cost: {}'.format(epoch+1, np.mean(costs)))
 
@@ -72,7 +75,7 @@ class RecurrentEBM(Algorithm):
         self.tf_session.run(init)
 
     def _build_model(self, n_visible):
-        self.input_data, self.lr, self.batch_size = self._create_placeholders(n_visible)
+        self.input_data, self.lr, self._batch_size = self._create_placeholders(n_visible)
         self.W, self.Wuh, self.Wux, self.Wxu, self.Wuu, self.bu, \
             self.u0, self.bh, self.bx, self.BH_t, self.BX_t = self._create_variables(n_visible)
 
@@ -92,16 +95,16 @@ class RecurrentEBM(Algorithm):
             bh_t = tf.add(self.bh, tf.matmul(u_tmin1, self.Wuh))
             return bh_t
 
-        self.BH_t = tf.tile(self.BH_t, [self.batch_size, 1])
-        self.BX_t = tf.tile(self.BX_t, [self.batch_size, 1])
+        self.BH_t = tf.tile(self.BH_t, [self._batch_size, 1])
+        self.BX_t = tf.tile(self.BX_t, [self._batch_size, 1])
 
         # Scan through the rnn and generate the value for each hidden node in the batch
         u_t = tf.scan(rnn_recurrence, self.input_data, initializer=self.u0)
         # Scan through the rnn and generate the visible and hidden biases for each RBM in the batch
         self.BX_t = tf.reshape(tf.scan(visible_bias_recurrence, u_t, tf.zeros([1, n_visible], tf.float32)),
-                               [n_visible, self.batch_size])
+                               [n_visible, self._batch_size])
         self.BH_t = tf.reshape(tf.scan(hidden_bias_recurrence, u_t, tf.zeros([1, self.n_hidden], tf.float32)),
-                               [self.n_hidden, self.batch_size])
+                               [self.n_hidden, self._batch_size])
 
         self.cost = self._run_ebm(self.input_data, self.W, self.BX_t, self.BH_t)
 
