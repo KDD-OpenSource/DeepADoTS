eceefcb1b281dcd282ce846d7f77f11786c6dbd3
Maxi Fischer <maxi.fischer@student.hpi.de>
2018-05-22 18:00:17 +0200

    revert main.py

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
-import numpy as np
-import tensorflow as tf
-import pandas as pd
-import math
-import tflearn
-import os
-import matplotlib.pyplot as plt
-from random import randint
-from sklearn import model_selection
-from time import time
-from scipy.stats import norm
-
-from DSEBM import *
-from LSTM_AD import *
-from evaluation import *
-from synthetic_data_generator import *
 from src.algorithms.dagmm import DAGMM
 from src.datasets.dataset import KDD_Cup
 from src.evaluation import get_accuracy_precision_recall_fscore
 
+
 def main():
+    dagmm = DAGMM()
+    kdd_cup = KDD_Cup()
+    (X_train, y_train), (X_test, y_test) = kdd_cup.get_data_dagmm()
+    dagmm.fit(X_train, y_train)
+    pred = dagmm.predict(X_test)
+    print(get_accuracy_precision_recall_fscore(y_test, pred))
+
 
 if __name__ == '__main__':
     main()
