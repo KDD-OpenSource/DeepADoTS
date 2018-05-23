6240ad07f8192912a65cf35bf5b880adbeddf947
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2018-05-23 10:51:40 +0200

    Adapt DAGMM dynamically to number of features

--- src/algorithms/dagmm.py
+++ src/algorithms/dagmm.py
@@ -1,16 +1,19 @@
 """Adapted from Daniel Stanley Tan (https://github.com/danieltan07/dagmm)"""
 
-import os
-import sys
-
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 from torch.utils.data import DataLoader
+
 from .algorithm import Algorithm
 
-sys.path.append(os.path.join(os.getcwd(), "third_party", "dagmm"))
-from third_party.dagmm.model import DaGMM  # noqa
-from third_party.dagmm.utils import to_var  # noqa
+
+def to_var(x, volatile=False):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x, volatile=volatile)
 
 
 class CustomDataLoader(object):
@@ -26,13 +29,160 @@ class CustomDataLoader(object):
         return np.float32(self.X[index])
 
 
+class DAGMM_Module(nn.Module):
+    """Residual Block."""
+
+    def __init__(self, n_features=118, n_gmm=2, latent_dim=3):
+        super(DAGMM_Module, self).__init__()
+
+        layers = []
+        layers += [nn.Linear(n_features, 60)]
+        layers += [nn.Tanh()]
+        layers += [nn.Linear(60, 30)]
+        layers += [nn.Tanh()]
+        layers += [nn.Linear(30, 10)]
+        layers += [nn.Tanh()]
+        layers += [nn.Linear(10, 1)]
+
+        self.encoder = nn.Sequential(*layers)
+
+        layers = []
+        layers += [nn.Linear(1, 10)]
+        layers += [nn.Tanh()]
+        layers += [nn.Linear(10, 30)]
+        layers += [nn.Tanh()]
+        layers += [nn.Linear(30, 60)]
+        layers += [nn.Tanh()]
+        layers += [nn.Linear(60, n_features)]
+
+        self.decoder = nn.Sequential(*layers)
+
+        layers = []
+        layers += [nn.Linear(latent_dim, 10)]
+        layers += [nn.Tanh()]
+        layers += [nn.Dropout(p=0.5)]
+        layers += [nn.Linear(10, n_gmm)]
+        layers += [nn.Softmax(dim=1)]
+
+        self.estimation = nn.Sequential(*layers)
+
+        self.register_buffer("phi", torch.zeros(n_gmm))
+        self.register_buffer("mu", torch.zeros(n_gmm, latent_dim))
+        self.register_buffer("cov", torch.zeros(n_gmm, latent_dim, latent_dim))
+
+    def relative_euclidean_distance(self, a, b):
+        return (a - b).norm(2, dim=1) / a.norm(2, dim=1)
+
+    def forward(self, x):
+
+        enc = self.encoder(x)
+
+        dec = self.decoder(enc)
+
+        rec_cosine = F.cosine_similarity(x, dec, dim=1)
+        rec_euclidean = self.relative_euclidean_distance(x, dec)
+
+        z = torch.cat([enc, rec_euclidean.unsqueeze(-1), rec_cosine.unsqueeze(-1)], dim=1)
+
+        gamma = self.estimation(z)
+
+        return enc, dec, z, gamma
+
+    def compute_gmm_params(self, z, gamma):
+        N = gamma.size(0)
+        # K
+        sum_gamma = torch.sum(gamma, dim=0)
+
+        # K
+        phi = (sum_gamma / N)
+
+        self.phi = phi.data
+
+        # K x D
+        mu = torch.sum(gamma.unsqueeze(-1) * z.unsqueeze(1), dim=0) / sum_gamma.unsqueeze(-1)
+        self.mu = mu.data
+        # z = N x D
+        # mu = K x D
+        # gamma N x K
+
+        # z_mu = N x K x D
+        z_mu = (z.unsqueeze(1) - mu.unsqueeze(0))
+
+        # z_mu_outer = N x K x D x D
+        z_mu_outer = z_mu.unsqueeze(-1) * z_mu.unsqueeze(-2)
+
+        # K x D x D
+        cov = torch.sum(gamma.unsqueeze(-1).unsqueeze(-1) * z_mu_outer, dim=0) / sum_gamma.unsqueeze(-1).unsqueeze(-1)
+        self.cov = cov.data
+
+        return phi, mu, cov
+
+    def compute_energy(self, z, phi=None, mu=None, cov=None, size_average=True):
+        if phi is None:
+            phi = to_var(self.phi)
+        if mu is None:
+            mu = to_var(self.mu)
+        if cov is None:
+            cov = to_var(self.cov)
+
+        k, D, _ = cov.size()
+
+        z_mu = (z.unsqueeze(1) - mu.unsqueeze(0))
+
+        cov_inverse = []
+        det_cov = []
+        cov_diag = 0
+        eps = 1e-12
+        for i in range(k):
+            # K x D x D
+            cov_k = cov[i] + to_var(torch.eye(D) * eps)
+            cov_inverse.append(torch.inverse(cov_k).unsqueeze(0))
+
+            det_cov.append(np.linalg.det(cov_k.data.cpu().numpy() * (2 * np.pi)))
+            cov_diag = cov_diag + torch.sum(1 / cov_k.diag())
+
+        # K x D x D
+        cov_inverse = torch.cat(cov_inverse, dim=0)
+        # K
+        det_cov = to_var(torch.from_numpy(np.float32(np.array(det_cov))))
+
+        # N x K
+        exp_term_tmp = -0.5 * torch.sum(torch.sum(z_mu.unsqueeze(-1) * cov_inverse.unsqueeze(0), dim=-2) * z_mu, dim=-1)
+        # for stability (logsumexp)
+        max_val = torch.max((exp_term_tmp).clamp(min=0), dim=1, keepdim=True)[0]
+
+        exp_term = torch.exp(exp_term_tmp - max_val)
+
+        sample_energy = -max_val.squeeze() - torch.log(
+            torch.sum(phi.unsqueeze(0) * exp_term / (torch.sqrt(det_cov)).unsqueeze(0), dim=1) + eps)
+
+        if size_average:
+            sample_energy = torch.mean(sample_energy)
+
+        return sample_energy, cov_diag
+
+    def loss_function(self, x, x_hat, z, gamma, lambda_energy, lambda_cov_diag):
+
+        recon_error = torch.mean((x - x_hat) ** 2)
+
+        phi, mu, cov = self.compute_gmm_params(z, gamma)
+
+        sample_energy, cov_diag = self.compute_energy(z, phi, mu, cov)
+
+        loss = recon_error + lambda_energy * sample_energy + lambda_cov_diag * cov_diag
+
+        return loss, sample_energy, recon_error, cov_diag
+
+
 class DAGMM(Algorithm):
 
     def __init__(self, lr=1e-4, batch_size=1024, gmm_k=4, normal_percentile=80):
         self.lr = lr
         self.batch_size = batch_size
         self.gmm_k = gmm_k  # Number of Gaussian mixtures
-        self.normal_percentile = normal_percentile  # Up to which percentile data should be consideres normal
+        self.normal_percentile = normal_percentile  # Up to which percentile data should be considers normal
+        self.dagmm, self.optimizer, self.train_phi, self.train_mu, self.train_cov, self.train_energy = \
+            None, None, None, None, None, None
 
     def _reset_grad(self):
         self.dagmm.zero_grad()
@@ -42,7 +192,7 @@ class DAGMM(Algorithm):
         Store the computed energy based on the training data and the aforementioned parameters."""
         data_loader = DataLoader(dataset=CustomDataLoader(X), batch_size=self.batch_size, shuffle=True)
 
-        self.dagmm = DaGMM(self.gmm_k)
+        self.dagmm = DAGMM_Module(n_features=X.shape[1], n_gmm=self.gmm_k)
         self.optimizer = torch.optim.Adam(self.dagmm.parameters(), lr=self.lr)
 
         if torch.cuda.is_available():
