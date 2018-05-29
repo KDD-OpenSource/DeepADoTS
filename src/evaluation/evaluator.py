a4527e104aab8c1d1ab168ece0212baebedca580
Maxi Fischer <maxi.fischer@student.hpi.de>
2018-05-29 11:04:53 +0200

    improve plots

--- src/evaluation/evaluator.py
+++ src/evaluation/evaluator.py
@@ -39,28 +39,36 @@ class Evaluator:
     def plot_scores(self):
         for ds in self.datasets:
             _, _, X_test, y_test = ds.get_data()
-            plt.subplot((2*len(self.detectors)+2) * 100 + 11)
+            subtitle_loc = 'left'
+            fig = plt.figure(figsize = (15, 15))
+            sp = fig.add_subplot((2*len(self.detectors)+2) * 100 + 11)
+            sp.set_title("original test set", loc = subtitle_loc)
             for col in X_test.columns:
                 plt.plot(X_test[col])
-            plt.subplot((2*len(self.detectors)+2) * 100 + 12)
+            sp = fig.add_subplot((2*len(self.detectors)+2) * 100 + 12)
+            sp.set_title("binary labels of test set", loc = subtitle_loc)
             plt.plot(y_test)
             
             subplot_num = 3
             for det in self.detectors:
-                plt.subplot((2*len(self.detectors)+2) * 100 + 10 + subplot_num)
+                sp = fig.add_subplot((2*len(self.detectors)+2) * 100 + 10 + subplot_num)
+                sp.set_title("scores of " + type(det).__name__, loc = subtitle_loc)
                 y_pred = self.results[(type(ds).__name__, type(det).__name__)]
                 plt.plot(np.arange(len(X_test)), [x for x in y_pred])
                 threshold_line = len(X_test) * [det.get_threshold(y_pred)]
                 plt.plot([x for x in threshold_line])
                 subplot_num += 1
                 
-                plt.subplot((2*len(self.detectors)+2) * 100 + 10 + subplot_num)
+                sp = fig.add_subplot((2*len(self.detectors)+2) * 100 + 10 + subplot_num)
+                sp.set_title("binary labels of " + type(det).__name__, loc = subtitle_loc)
                 plt.plot(np.arange(len(X_test)), [x for x in det.get_binary_label(y_pred)])
                 subplot_num += 1
         plt.legend()
+        plt.tight_layout()
         plt.show()
         
-        self.plot_roc_curves()
+        #TODO: fix
+        #self.plot_roc_curves()
         
     def plot_roc_curves(self):
         #Plot of a ROC curve for all classes
