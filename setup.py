7efcd8d500b22419127523345bcc11b6fcd78d5c
Thomas Kellermeier <Chaoste@users.noreply.github.com>
2018-05-30 08:21:51 +0200

    Thomas/setup (#1)
    Add setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+setup(
+    name='mp-2018',
+    author='Maxi Fischer, Willi Gierke, Ajay Kesar, Thomas Kellermeier, Axel Stebner, Daniel Thevessen',
+    description='Unsupervised Anomaly Detection: Representation Learning for Predictive Maintenance over Time',
+    long_description=open('README.md').read(),
+    version='0.0',
+    packages=[],
+    scripts=[],
+    # Requirements for executing the project (not development)
+    install_requires=parse_requirements('requirements.txt'),
+    url='github.com/KDD-OpenSource/MP-2018',
+    license='MIT License',
+)
