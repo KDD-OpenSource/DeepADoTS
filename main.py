bf6c3d1f1cf6fbcd272f78d8547044fb6c964b78
Thomas Kellermeier <t-kellermeier@hotmail.de>
2018-05-30 23:23:58 +0200

    Cleanup main

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
-# import pandas as pd
-# import numpy as np
-# from src.algorithms.donut import Donut
-# from src.datasets.air_quality import AirQuality
-from src.datasets import SyntheticDataGenerator
-from src.algorithms import RecurrentEBM
-from src.algorithms import LSTMAD
+import pickle
+
+import pandas as pd
+import numpy as np
+
+from src.datasets import AirQuality, SyntheticData, KDDCup
+from src.algorithms import DAGMM, Donut, RecurrentEBM, LSTMAD
 from src.evaluation.evaluator import Evaluator
 
 
 def main():
-    # dagmm = DAGMM()
-    # kdd_cup = KDD_Cup()
-    # (X_train, y_train), (X_test, y_test) = kdd_cup.get_data_dagmm()
-    # dagmm.fit(X_train, y_train)
-    # pred = dagmm.predict(X_test)
-    # print(get_accuracy_precision_recall_fscore(y_test, pred))
+    # execute_dagmm()
+    # execute_donut()
+    # execute_pipeline()
 
-    # donut = Donut()
-    # air_quality = AirQuality().get_data()
-    # X = air_quality.loc[:, [air_quality.columns[2], "timestamps"]]
-    # X["timestamps"] = X.index
-    # split_ratio = 0.8
-    # split_point = int(split_ratio * len(X))
-    # X_train = X[:split_point]
-    # X_test = X[split_point:]
-    # y_train = pd.Series(0, index=np.arange(len(X_train)))
-    # donut.fit(X_train, y_train)
-    # pred = donut.predict(X_test)
-    # print(pred)
 
-    datasets = [SyntheticDataGenerator.extreme_1()]
+def execute_pipeline():
+    datasets = [SyntheticData("Synthetic Extreme Outliers", ".")]
     detectors = [RecurrentEBM(num_epochs=15), LSTMAD()]
     evaluator = Evaluator(datasets, detectors)
     evaluator.evaluate()
     df = evaluator.benchmarks()
-    print(df)
+    print('Evaluated benchmarks: ', df)
     evaluator.plot_scores()
+
+
+def execute_donut():
+    donut = Donut()
+    air_quality = AirQuality().get_data()
+    X = air_quality.loc[:, [air_quality.columns[2], "timestamps"]]
+    X["timestamps"] = X.index
+    split_ratio = 0.8
+    split_point = int(split_ratio * len(X))
+    X_train = X[:split_point]
+    X_test = X[split_point:]
+    y_train = pd.Series(0, index=np.arange(len(X_train)))
+    donut.fit(X_train, y_train)
+    pred = donut.predict(X_test)
+    print("Donut results: ", pred)
+
+
+def execute_dagmm():
+    dagmm = DAGMM()
+    kdd_cup = KDDCup()
+    (X_train, y_train), (X_test, y_test) = kdd_cup.get_data_dagmm()
+    dagmm.fit(X_train, y_train)
+    pred = dagmm.predict(X_test)
+    print("DAGMM results: ", Evaluator.get_accuracy_precision_recall_fscore(y_test, pred))
 
 
 if __name__ == '__main__':
