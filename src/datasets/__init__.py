95f445fc5bccfbb59eb731cb0392f71706d8329b
Willi Gierke <willi.gierke@student.hpi.de>
2018-05-30 18:38:54 +0200

    Remove MissingValuesDataset

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,6 +1,6 @@
 from .air_quality import AirQuality
 from .kdd_cup import KDDCup
-from .synthetic_data_generator import SyntheticDataGenerator, SyntheticData, MissingValuesDataset
+from .synthetic_data_generator import SyntheticDataGenerator, SyntheticData
 
 
-__all__ = ['AirQuality', 'KDDCup', 'SyntheticDataGenerator', 'SyntheticData', 'MissingValuesDataset']
+__all__ = ['AirQuality', 'KDDCup', 'SyntheticDataGenerator', 'SyntheticData']
