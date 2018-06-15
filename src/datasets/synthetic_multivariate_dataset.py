2d07d71dcc274a15252ae2af8f055ec35edbae0e
Thomas Kellermeier <t-kellermeier@hotmail.de>
2018-06-15 18:38:32 +0200

    Refactor mutlivariate dataset and add new dim2 functions

--- src/datasets/synthetic_multivariate_dataset.py
+++ src/datasets/synthetic_multivariate_dataset.py
@@ -3,67 +3,127 @@ import pandas as pd
 
 from .dataset import Dataset
 
+"""
+TODO:
+- Parameter n_dim -> Variable Dimensionen (modulo 2)
+- Type=[Inverse, Factorized, Outlier in both dimension with specific delay]
+"""
+
+def add_noise(self, x, strength = 1):
+    return x + np.random.random(np.shape(x)) * strength - strength / 2
+
+# ----- Functions generating the second dimension --------- #
+
+def doubled_dim2(self, curve_values, anomalous):
+    factor = 4 if anomalous else 2
+    return curve_values * factor
+
+
+def inversed_dim2(self, curve_values, anomalous):
+    factor = -2 if anomalous else 2
+    return curve_values * factor
+
+
+def shrinked_dim2(self, curve_values, anomalous):
+    if not anomalous:
+        return curve_values * 2
+    else:
+        new_curve = curve_values[::2]
+        nonce = np.zeros(len(curve_values) - len(new_curve))
+        return nonce + new_curve
+
+
+def delayed_dim2(self, curve_values, anomalous):
+    if not anomalous:
+        return curve_values * 2
+    else:
+        # The curve in the second dimension occures a few timestamps later
+        nonce = np.zeros(len(curve_values) // 10)
+        # TODO: generate method should support different arrays of various length
+        return nonce + curve_values
 
 class SyntheticMultivariateDataset(Dataset):
 
-    def __init__(self, name="Synthetic Multivariate Curve Outliers"):
-        super().__init__(name, file_name='synthetic_mv1.pkl')
+    def __init__(self, name: string = 'Synthetic Multivariate Curve Outliers',
+                 length: int = 5000,
+                 mean_curve_length: int = 40,  # varies between -5 and +5
+                 mean_curve_amplitude: int = 1  # By default varies between -0.5 and 1.5
+                 dim2=doubled_dim2,  # dim2: Lambda for curve values of 2nd dimension
+                 random_seed: int = 42,
+                 file_name: string = 'synthetic_mv1.pkl'):
+        super().__init__(name, file_name)
+        self.length = length
+        self.mean_curve_length = mean_curve_length
+        self.mean_curve_amplitude = mean_curve_amplitude
+        self.global_noise = 0.1  # Noise added to all dimensions over the whole timeseries
+        self.dim2 = dim2
+        self.random_seed = random_seed
 
-    def add_noise(self, x, strength=1):
-        return x + np.random.random(np.shape(x)) * strength - strength / 2
-
+    # Use part of sinus to create a curve starting and ending with zero gradients.
+    # Using `length` and `amplitude` you can adjust it in both dimensions.
     def get_curve(self, length, amplitude):
+        # Transformed sinus curve: [-1, 1] -> [0, amplitude]
         curve = lambda t: amplitude * (np.sin(t)/2 + 0.5)
+        # Start and end of one curve section in sinus
         from_ = 1.5 * np.pi
         to_ = 3.5 * np.pi
         return np.array([curve(t) for t in np.linspace(from_, to_, length)])
 
-    def get_random_curve(self, length, amplitude, length_randomness=10, amplitude_randomness=1, negative=False):
+    # Randomly adjust curve size by adding noise to the passed parameters
+    def get_random_curve(self, negative=False, length_randomness=10, amplitude_randomness=1):
         neg = -1 if negative else 1
-        new_length = int(self.add_noise(length, length_randomness))
-        new_amplitude = self.add_noise(neg * amplitude, amplitude_randomness)
+        new_length = int(add_noise(self.mean_curve_length, length_randomness))
+        new_amplitude = add_noise(neg * self.mean_curve_amplitude, amplitude_randomness)
         return self.get_curve(new_length, new_amplitude)
 
-    def generate_clean_data(self, T=1000, margin_factor = 6, anomalous=False, pollution=0.5):
-        # T: amount of timestamps
-        pause = lambda: 5 + np.random.randint(70)  # Function for returning a pause interval
-        mean_curve_length = 40
-        mean_curve_amplitude = 1
-        dim2 = lambda x: x * 2
-        anomalous_dim2 = lambda x: x * self.add_noise(4)
-        noise = 0.1
+    # The interval between two curves must be random so a detector doesn't recognize a pattern
+    def create_pause(xmin=5, xmax=75):
+        diff = xmax - xmin
+        return xmin + np.random.randint(diff)
 
-        values = np.zeros((T, 2))
-        labels = np.zeros(T)
-        pos = pause()
+    """
+        padding_factor: Add a padding to the anomaly labels so they don't start with
+            the first curve value. The padding is curve_length / padding_factor
+        pollution: Portion of anomalous curves. Because it's not known how many curves there are
+            in the end. It's randomly chosen based on this value. To avoid anomalies set this to zero.
+    """
+    def generate_data(self, padding_factor=6, pollution=0.5):
+        features = 2
 
-        # first pos data points are noise
-        values[:pos] = self.add_noise(values[:pos], noise)
+        values = np.zeros((self.length, features))
+        labels = np.zeros(self.length)
+        pos = self.create_pause()
 
-        while pos < T - mean_curve_length - 20:
-            # general outline for the repeating curves, only changes heights
-            curve = self.get_random_curve(mean_curve_length, mean_curve_amplitude, negative=np.random.choice([True, False]))
-            # outlier generation in second dimension
-            create_anomaly = anomalous and np.random.choice([0, 1], p=[1-pollution, pollution])
-            values[pos:pos+len(curve), 0] = self.add_noise(curve, noise)
-            values[pos:pos+len(curve), 1] = self.add_noise(anomalous_dim2(curve) if create_anomaly else dim2(curve), noise)
-            # add labels with slight margin
+        # First pos data points are noise (don't start directly with curve)
+        values[:pos] = add_noise(values[:pos], self.global_noise)
+
+        while pos < self.length - self.mean_curve_length - 20:
+            # General outline for the repeating curves, only changes heights and length
+            is_negative = np.random.choice([True, False])
+            curve = self.get_random_curve(is_negative)
+            # Outlier generation in second dimension
+            create_anomaly = np.random.choice([False, True], p=[1-pollution, pollution])
+            # Before storing in timeline add a noise
+            values[pos:pos+len(curve), 0] = add_noise(curve, self.global_noise)
+            # dim2 function gets the clean curve values (not noisy)
+            values[pos:pos+len(curve), 1] = add_noise(
+                self.dim2(curve, create_anomaly), self.global_noise)
+            # Add anomaly labels with slight padding (dont start with the first value)
             if create_anomaly:
-                margin = len(curve) // margin_factor
-                labels[pos+margin:pos+len(curve)-margin] += 1
+                padding = len(curve) // padding_factor
+                labels[pos+padding:pos+len(curve)-padding] += 1
             pos += len(curve)
-            # add pause without curve, only noise
-            pause_length = pause()
-            values[pos:pos+pause_length] = self.add_noise(values[pos:pos+pause_length], noise)
+            # Add pause without curve, only noise
+            pause_length = self.create_pause()
+            values[pos:pos+pause_length] = add_noise(
+                values[pos:pos+pause_length], self.global_noise)
             pos += pause_length
         # rest of values is noise
-        values[pos:] = self.add_noise(values[pos:], noise)
+        values[pos:] = add_noise(values[pos:], self.global_noise)
         return pd.DataFrame(values), pd.Series(labels)
 
     def load(self):
-        # T: amount of timestamps
-        T = 5000
-        np.random.seed(42)
-        X_train, y_train = self.generate_clean_data(T)
-        X_test, y_test = self.generate_clean_data(T, anomalous=True)
+        np.random.seed(self.random_seed)
+        X_train, y_train = self.generate_data(pollution=0)
+        X_test, y_test = self.generate_data(pollution=0.5)
         self._data = X_train, y_train, X_test, y_test
