2d02b0a2dcd12d13d329c5ab2368f2900ddf4703
Daniel Theveßen <danthe96@gmx.de>
2018-06-20 22:01:00 +0200

    Fix crashes in evaluator

--- src/evaluation/evaluator.py
+++ src/evaluation/evaluator.py
@@ -41,6 +41,8 @@ class Evaluator:
 
     @staticmethod
     def get_auroc(det, ds, score):
+        if np.isnan(score).all():
+            score = np.zeros_like(score)
         _, _, _, y_test = ds.data()
         score_nonan = score.copy()
         # Rank NaN below every other value in terms of anomaly score
@@ -191,6 +193,8 @@ class Evaluator:
             for det in self.detectors:
                 self.logger.info(f"Plotting ROC curve for {det.name} on {ds.name}")
                 score = self.results[(ds.name, det.name)]
+                if np.isnan(score).all():
+                    score = np.zeros_like(score)
                 # Rank NaN below every other value in terms of anomaly score
                 score[np.isnan(score)] = np.nanmin(score) - sys.float_info.epsilon
                 fpr, tpr, _ = roc_curve(y_test, score)
