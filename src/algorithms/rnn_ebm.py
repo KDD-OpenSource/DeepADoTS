5f8e1654182e10b9ab9165d3bb354ca8d1b1cd0c
Ajay <you@example.com>
2018-07-19 16:42:49 +0200

    removing false score handling

--- src/algorithms/rnn_ebm.py
+++ src/algorithms/rnn_ebm.py
@@ -59,7 +59,6 @@ class RecurrentEBM(Algorithm, GPUWrapper):
                                                          feed_dict={self.input_data: X[i:i + 1],
                                                                     self._batch_size: 1})
                 scores.append(reconstruction_err[0])
-            scores = np.square(np.array(scores) - np.mean(scores))
             if self.min_energy is not None:
                 labels = np.where(scores >= self.min_energy)
 
