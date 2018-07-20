11e1aded2ebc4b32e781cdf4f6c65f88a6a70b38
Willi Gierke <willi.gierke@student.hpi.de>
2018-07-20 09:30:29 +0200

    Move up function

--- main.py
+++ main.py
@@ -24,35 +24,6 @@ def main():
     # evaluate_real_datasets()
 
 
-def evaluate_real_datasets():
-    REAL_DATASET_GROUP_PATH = "data/raw/"
-    real_dataset_groups = glob.glob(REAL_DATASET_GROUP_PATH + "*")
-    seeds = np.random.randint(low=0, high=2 ** 32 - 1, size=RUNS, dtype="uint32")
-    results = pd.DataFrame()
-    datasets = [KDDCup(seed=1)]
-    for real_dataset_group in real_dataset_groups:
-        for data_set_path in glob.glob(real_dataset_group + "/labeled/train/*"):
-            data_set_name = data_set_path.split('/')[-1].replace('.pkl', '')
-            dataset = RealPickledDataset(data_set_name, data_set_path)
-            datasets.append(dataset)
-
-    for seed in seeds:
-        datasets[0] = KDDCup(seed)
-        evaluator = Evaluator(datasets, detectors, seed=seed)
-        evaluator.evaluate()
-        result = evaluator.benchmarks()
-        evaluator.plot_roc_curves()
-        evaluator.plot_threshold_comparison()
-        evaluator.plot_scores()
-        results = results.append(result, ignore_index=True)
-
-    avg_results = results.groupby(["dataset", "algorithm"], as_index=False).mean()
-    evaluator.set_benchmark_results(avg_results)
-    evaluator.export_results('run_real_datasets')
-    evaluator.create_boxplots(runs=RUNS, data=results, detectorwise=False)
-    evaluator.create_boxplots(runs=RUNS, data=results, detectorwise=True)
-
-
 def detectors():
     if os.environ.get("CIRCLECI", False):
         dets = [RecurrentEBM(num_epochs=2), Donut(num_epochs=5, x_dims=30), LSTMAD(num_epochs=5), DAGMM(num_epochs=2),
@@ -168,6 +139,35 @@ def run_final_missing_experiment(outlier_type='extreme_1', runs=25, output_dir=N
                                steps=steps, store_results=False)
     plotter = Plotter('reports', output_dir)
     plotter.plot_experiment('missing on extreme_1')
+
+
+def evaluate_real_datasets():
+    REAL_DATASET_GROUP_PATH = "data/raw/"
+    real_dataset_groups = glob.glob(REAL_DATASET_GROUP_PATH + "*")
+    seeds = np.random.randint(low=0, high=2 ** 32 - 1, size=RUNS, dtype="uint32")
+    results = pd.DataFrame()
+    datasets = [KDDCup(seed=1)]
+    for real_dataset_group in real_dataset_groups:
+        for data_set_path in glob.glob(real_dataset_group + "/labeled/train/*"):
+            data_set_name = data_set_path.split('/')[-1].replace('.pkl', '')
+            dataset = RealPickledDataset(data_set_name, data_set_path)
+            datasets.append(dataset)
+
+    for seed in seeds:
+        datasets[0] = KDDCup(seed)
+        evaluator = Evaluator(datasets, detectors, seed=seed)
+        evaluator.evaluate()
+        result = evaluator.benchmarks()
+        evaluator.plot_roc_curves()
+        evaluator.plot_threshold_comparison()
+        evaluator.plot_scores()
+        results = results.append(result, ignore_index=True)
+
+    avg_results = results.groupby(["dataset", "algorithm"], as_index=False).mean()
+    evaluator.set_benchmark_results(avg_results)
+    evaluator.export_results('run_real_datasets')
+    evaluator.create_boxplots(runs=RUNS, data=results, detectorwise=False)
+    evaluator.create_boxplots(runs=RUNS, data=results, detectorwise=True)
 
 
 if __name__ == '__main__':
