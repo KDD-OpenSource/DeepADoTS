6d2aa5f0070432d7278bb9f083af76c7b4d8231d
Thomas Kellermeier <t-kellermeier@hotmail.de>
2018-07-26 18:37:11 +0200

    refactor plotter

--- src/evaluation/plotter.py
+++ src/evaluation/plotter.py
@@ -51,6 +51,8 @@ class Plotter:
                     self.logger.warn('benchmark_results was None')
         return all_results
 
+    # --- Final plot functions ----------------------------------------------- #
+
     # results is an array of benchmark_results (e.g. returned by get_results_for_runs)
     def barplots(self, title):
         if len(self.detector_names) == 1:
@@ -60,27 +62,8 @@ class Plotter:
 
         fig, axes = plt.subplots(
             ncols=len(self.detector_names), figsize=(1.5*len(self.detector_names), 4), sharey=True)
-
-        if len(self.detector_names) == 1:
-            axes = np.array(axes)
-
         for ax, det in zip(axes.flat, self.detector_names):
-            values = aurocs_df[aurocs_df['algorithm'] == det].drop(columns='algorithm')
-            ds_groups = values.groupby('dataset')
-            ax.boxplot([ds_groups.get_group(x)['auroc'].values for x in self.dataset_names],
-                       positions=np.linspace(0, 1, 5), widths=0.15,
-                       medianprops={'linewidth': 1},
-                       whiskerprops={'color': 'darkblue', 'linestyle':'--'},
-                       flierprops={'markersize': 3},
-                       boxprops={'color': 'darkblue'}
-            )
-            ax.set_xticklabels([f'{float(Evaluator.get_key_and_value(x)[1]):.2f}' for x in self.dataset_names], rotation=90)
-
-            ax.set_xlabel(NAMES_TRANSLATION.get(det, det))
-            ax.set_ylim((0, 1.05))
-            ax.yaxis.grid(True)
-            ax.set_xlim((-0.15, 1.15))
-            ax.margins(0.05)
+            self._styled_boxplot(ax, aurocs_df, det)
 
         fig.subplots_adjust(wspace=0)
         fig.suptitle(f'Area under ROC for {title} (runs={len(self.results)})')
@@ -92,23 +75,9 @@ class Plotter:
         aurocs_df = pd.concat(aurocs, axis=0, ignore_index=True)
         det = self.detector_names[0]
 
-        fig, ax = plt.subplots(ncols=1, figsize=(4, 4))
-
-        values = aurocs_df[aurocs_df['algorithm'] == det].drop(columns='algorithm')
-        ds_groups = values.groupby('dataset')
-        ax.boxplot([ds_groups.get_group(x)['auroc'].values for x in self.dataset_names],
-                   positions=np.linspace(0, 1, 5), widths=0.15,
-                   medianprops={'linewidth': 1},
-                   whiskerprops={'color': 'darkblue', 'linestyle':'--'},
-                   flierprops={'markersize': 3},
-                   boxprops={'color': 'darkblue'}
-        )
-        ax.set_xticklabels([f'{float(Evaluator.get_key_and_value(x)[1]):.2f}' for x in self.dataset_names])
-
-        ax.set_ylim((0, 1.05))
-        ax.yaxis.grid(True)
-        ax.set_xlim((-0.15, 1.15))
-        ax.margins(0.05)
+        fig, ax = plt.subplots(figsize=(4, 4))
+        self._styled_boxplot(ax, aurocs_df, det)
+        ax.set_xlabel(None)
 
         final_det_name = NAMES_TRANSLATION.get(det, det)
         fig.subplots_adjust(wspace=0)
@@ -116,6 +85,33 @@ class Plotter:
         self.store(fig, f'boxplot-{final_det_name}-{title}', 'pdf', bbox_inches='tight')
         return fig
 
+    def lineplot(self, title):
+        self.logger.warn('Final lineplot function is not implemented')
+        pass
+
+    def heatmap(self, title):
+        self.logger.warn('Final heatmap function is not implemented')
+        pass
+
+    # --- Helper functions --------------------------------------------------- #
+
+    def _styled_boxplot(self, ax, aurocs_df, det):
+        values = aurocs_df[aurocs_df['algorithm'] == det].drop(columns='algorithm')
+        ds_groups = values.groupby('dataset')
+        ax.boxplot([ds_groups.get_group(x)['auroc'].values for x in self.dataset_names],
+                   positions=np.linspace(0, 1, 5), widths=0.15,
+                   medianprops={'linewidth': 1},
+                   whiskerprops={'color': 'darkblue', 'linestyle': '--'},
+                   flierprops={'markersize': 3},
+                   boxprops={'color': 'darkblue'})
+        ax.set_xticklabels([f'{float(Evaluator.get_key_and_value(x)[1]):.2f}' for x in self.dataset_names],
+                           rotation=90)
+
+        ax.set_xlabel(NAMES_TRANSLATION.get(det, det))
+        ax.set_ylim((0, 1.05))
+        ax.yaxis.grid(True)
+        ax.set_xlim((-0.15, 1.15))
+        ax.margins(0.05)
 
     def store(self, fig, title, extension='pdf', **kwargs):
         timestamp = time.strftime('%Y-%m-%d-%H%M%S')
