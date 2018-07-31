bfccc39620544ead18ff5e8711852052d957d419
Thomas Kellermeier <t-kellermeier@hotmail.de>
2018-07-31 23:35:59 +0200

    put legend outside of the plot

--- src/evaluation/plotter.py
+++ src/evaluation/plotter.py
@@ -95,7 +95,11 @@ class Plotter:
         ax.set_ylabel('AUROC')
         ax.set_ylim((0, 1.05))
         ax.yaxis.grid(True)
-        ax.legend()
+        # Shrink current axis by 20%
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        # Put a legend to the right of the current axis
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
         fig.subplots_adjust(wspace=0)
         fig.suptitle(f'Area under ROC for {title} (runs={len(self.results)})')
         self.store(fig, f'lineplot-experiment-{title}', 'pdf', bbox_inches='tight')
